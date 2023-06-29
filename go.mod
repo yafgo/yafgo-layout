7682a6eac1c6ba148b3dc98dea4c7eb65aa61cf8
ange <forever_youyou@foxmail.com>
2023-06-29 17:17:15 +0800

    addPkg: config

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module go-toy/toy-layout
 go 1.20
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/gookit/color v1.5.3
 	github.com/spf13/cobra v1.7.0
+	github.com/spf13/viper v1.16.0
+	github.com/stretchr/testify v1.8.4
 	gorm.io/driver/mysql v1.4.4
 	gorm.io/gen v0.3.22
 	gorm.io/gorm v1.24.2
@@ -11,14 +15,28 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
-	golang.org/x/tools v0.1.12 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/datatypes v1.1.1-0.20230130040222-c43177d3cf8c // indirect
 	gorm.io/hints v1.1.0 // indirect
 )
---
+++ pkg/config/application.go
@@ -0,0 +1,97 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/gookit/color"
+	"github.com/spf13/viper"
+)
+
+var conf *viper.Viper
+var (
+	envPrefix = ""
+	configDir = "."
+)
+
+func Config() *viper.Viper {
+	if conf == nil {
+		color.Errorln("Config尚未初始化")
+		os.Exit(1)
+	}
+	return conf
+}
+
+func SetEnvPrefix(val string) {
+	envPrefix = val
+}
+
+func SetConfigDir(val string) {
+	configDir = val
+}
+
+// SetupViper 初始化配置
+//
+//	配置加载顺序: [mode].yaml -> [mode].local.yaml
+//	故配置优先级: [mode].local.yaml  >  [mode].yaml
+//	可以自定义任意 mode, 只需要新增 [mode].yaml 文件即可
+func SetupConfig(mode string) *viper.Viper {
+	var configName string
+
+	// 初始化默认配置
+	conf = viper.New()
+	conf.SetConfigType("yaml")
+	conf.AddConfigPath(configDir)
+
+	// 读取环境变量, 设置前缀, 后续获取的时候不需要加前缀
+	//   如前缀为 MYAPP 时: export MYAPP_WS_ADDR=ws://127.0.0.1:8080
+	//   _viper.GetString("WS_ADDR") 即可获取
+	//   _viper.GetString("ws_addr") 也可以获取
+	conf.SetEnvPrefix(envPrefix)
+	conf.AutomaticEnv()
+
+	defer func() {
+		// 监听配置文件变化
+		conf.WatchConfig()
+		conf.OnConfigChange(func(e fsnotify.Event) {
+			color.Infoln("config file changed:", e.Name)
+		})
+	}()
+
+	// 1. 读取指定 mode 配置
+	if mode == "" {
+		panic("Error: mode is empty")
+	}
+	if _f := filepath.Join(configDir, mode+".yaml"); fileExist(_f) {
+		configName = mode
+		// 读取指定 mode 配置
+		conf.SetConfigName(configName)
+		color.Successln("MergeInConfig:", configName+".yaml")
+		if err := conf.MergeInConfig(); err != nil {
+			panic(fmt.Errorf("MergeInConfig Error [%s]: %+v", configName, err))
+		}
+	} else {
+		panic(fmt.Sprintf("Error: [%s] not exist", configName))
+	}
+
+	// 2. [如果local配置存在]
+	// 读取 local 配置, 不进版本库, 如: dev.local.yaml, prod.local.yaml
+	if _f := filepath.Join(configDir, mode+".local.yaml"); fileExist(_f) {
+		configName = mode + ".local"
+		// 读取 local 配置
+		conf.SetConfigName(configName)
+		color.Successln("MergeInConfig:", configName+".yaml")
+		if err := conf.MergeInConfig(); err != nil {
+			panic(fmt.Errorf("MergeInConfig Error [%s]: %+v", configName, err))
+		}
+	}
+
+	return conf
+}
+
+func fileExist(path string) bool {
+	_, err := os.Lstat(path)
+	return !os.IsNotExist(err)
+}
