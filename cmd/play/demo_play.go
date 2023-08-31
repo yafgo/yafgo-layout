9c65d122aa986ae4e38ff8e2b0b200d3101b22ef
ange <forever_youyou@foxmail.com>
2023-08-31 11:06:35 +0800

    add play subCmd: gorm demo

--- cmd/play/demo_play.go
+++ cmd/play/demo_play.go
@@ -1,7 +1,12 @@
 package play
 
 import (
+	"context"
 	"fmt"
+	"yafgo/yafgo-layout/internal/global"
+	"yafgo/yafgo-layout/internal/model"
+	"yafgo/yafgo-layout/internal/query"
+	"yafgo/yafgo-layout/pkg/sys/ylog"
 
 	"github.com/spf13/cobra"
 )
@@ -10,6 +15,7 @@ import (
 func init() {
 	addSubPlayCommand(
 		demo,
+		demoGorm,
 	)
 }
 
@@ -18,5 +24,34 @@ var demo = &cobra.Command{
 	Short: "play demo演示",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("play demo")
+	},
+}
+
+var demoGorm = &cobra.Command{
+	Use:   "gorm",
+	Short: "gorm测试",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		ylog.Debug(ctx, "moduleName string")
+		res := make([]map[string]any, 0)
+		tx := global.Mysql.Raw("show tables").Find(&res)
+		if tx.Error != nil {
+			ylog.Errorf(ctx, "err: %v", tx.Error)
+			return
+		}
+		ylog.Debug(ctx, res)
+
+		user := &model.User{
+			Name: "张三",
+		}
+		userDO := query.User.WithContext(ctx)
+		userDO.Create(user)
+		ylog.Debug(ctx, user)
+
+		user, _ = userDO.First()
+		ylog.Debug(ctx, user)
+
+		user, _ = userDO.First()
+		ylog.Debug(ctx, user)
 	},
 }
